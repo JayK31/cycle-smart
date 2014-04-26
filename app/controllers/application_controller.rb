c0f8ab9b59f17bd19219ff0f421e6bc39fbfad29
tliounyc <tom.liou@gmail.com>
2014-04-26 14:50:35 -0400

    added logged in and current user helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  # declare a method as a helper
+  # http://api.rubyonrails.org/classes/AbstractController/Helpers/ClassMethods.html#method-i-helper_method
+  # which means we can use the methods in the view as well
+  helper_method(:current_user, :logged_in?)
+
 end
