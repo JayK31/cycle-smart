92d52427a2fb11a7b34e91323085fe8317a1d82f
JayK31 <jay.kaye31@gmail.com>
2014-04-26 17:59:42 -0400

    added basics user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,24 @@
 class User < ActiveRecord::Base
+  # basic validations
+  validates :name, presence: true
+  # email must be unique, not case sensitive
+  validates :email, presence: true, uniqueness: true, case_sensitive: false
+  # pw length between 8-16 chars
+  validates_length_of :password, in: 8..16
+  # basic validation of password format
+  validates_format_of :email, :with => /@/
   has_secure_password
 
+end
+
+class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, case_sensitive: false
+  validates_length_of :password, in: 8..16
+  validates_format_of :email, :with => /@/
+  has_secure_password
+  has_many :trips
+  has_many :items, :through => :trips
+  has_many :invites
+  has_many :trips, :through => :invites
 end
