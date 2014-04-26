cf851fd058953d1a44ce2e40395c226b2e20fc6d
JayK31 <jay.kaye31@gmail.com>
2014-04-26 14:21:09 -0400

    create edit, update, delete actions for user with edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
 
-  def index
+  def edit
+    # find the user we want to edit by id
+    # this will send the @user to the edit page with a form
+    @user = User.find(params[:id])
+  end
+
+  def update
+  end
+
+  def delete
   end
 
 end
