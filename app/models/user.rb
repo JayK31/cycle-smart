9fec5dec57ec47d7fcefd74b678bba9545d502e4
JayK31 <jay.kaye31@gmail.com>
2014-04-27 16:23:59 -0400

    clean up user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,4 @@ class User < ActiveRecord::Base
   # basic validation of password format
   validates_format_of :email, :with => /@/
   has_secure_password
-
-end
-
-class User < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, case_sensitive: false
-  validates_length_of :password, in: 8..16
-  validates_format_of :email, :with => /@/
-  has_secure_password
-  has_many :trips
-  has_many :items, :through => :trips
-  has_many :invites
-  has_many :trips, :through => :invites
 end
