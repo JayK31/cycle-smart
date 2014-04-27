3a0252d10a651e6238f33ddd48ecc2f7ca349322
JayK31 <jay.kaye31@gmail.com>
2014-04-27 18:59:09 -0400

    fix spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
 
-
-=======
->>>>>>> start_capybara_tests
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-<<<<<<< HEAD
-=======
 require 'capybara/rails'
->>>>>>> start_capybara_tests
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -19,11 +12,9 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-<<<<<<< HEAD
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
-=======
 ActiveRecord::Migration.maintain_test_schema!
->>>>>>> start_capybara_tests
+
 
 RSpec.configure do |config|
   # ## Mock Framework
