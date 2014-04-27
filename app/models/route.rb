ae5e7bfd1134aef898c41d0a5e78260ea6bca00d
isaacsaqib <isaac.saqib@stonybrook.edu>
2014-04-27 17:02:41 -0400

    added annotations

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: routes
+#
+#  id           :integer          not null, primary key
+#  start_point  :string(255)
+#  end_point    :string(255)
+#  safety_score :integer
+#  description  :text
+#  created_at   :datetime
+#  updated_at   :datetime
+#  user_id      :integer
+#
+
 class Route < ActiveRecord::Base
 end
