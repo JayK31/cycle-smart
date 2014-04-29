72c9f1b261a49145808232abf7d5eacd9f52c85b
JayK31 <jay.kaye31@gmail.com>
2014-04-29 13:51:53 -0400

    add shop controller and create index

---
+++ app/controllers/shop_controller.rb
@@ -0,0 +1,13 @@
+class ShopController < ApplicationController
+
+  def index
+    @shops = Shop.fetch
+
+    respond_to do |format|
+      format.html {}
+      format.json { render json: @shops }
+    end
+
+  end
+
+end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,8 @@
 class Shop
 
+  def self.fetch
+    # send request to google looking for bike ships in NYC with a radius of 5 miles and our own app API key
+    raw_shops = HTTParty.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=bike+shops+in+New+York+City&radius=5&key=gfbdCHpVPWyIudeY3ME5MXKJ')
+  end
 
 end
