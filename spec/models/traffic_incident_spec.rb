11a16dff1f622fb421320268b9781483e877a7fd
JayK31 <jay.kaye31@gmail.com>
2014-04-28 21:02:57 -0400

    fixed tests

--- spec/models/traffic_incident_spec.rb
+++ spec/models/traffic_incident_spec.rb
@@ -1,27 +1,26 @@
 require 'spec_helper'
 
 describe TrafficIncident do 
-  let(:new_traffic_incident) { TrafficIncident.new( JSON.parse('["40.684626","-73.938369","JEFFERSON AVENUE","MARCUS GARVEY BOULEVARD",[1,1,3,[0,0],[1,0],[1,0],[0,0],[2,0],[],[]],2,[0,1,2,[0,0],[0,0],[0,0],[0,0],[0,0],[["passenger_vehicle",1],["taxi_vehicle",1]],[]],2,[0,1,2,[0,0],[0,0],[0,0],[0,0],[0,0],[["van",1],["passenger_vehicle",1]],[]],[0,1,2,[0,0],[1,0],[1,0],[0,0],[2,0],[["sport_utility_station_wagon",1],["passenger_vehicle",2],["unknown",1]],[]],[1,1,4,[0,0],[0,0],[2,0],[0,0],[2,0],[["van",1],["taxi_vehicle",1]],[]],1,[1,1,2,[1,0],[0,0],[0,0],[0,0],[1,0],[["bicycle",1],["motorcycle",1]],[]],6,[1,1,4,[0,0],[0,0],[2,0],[0,0],[2,0],[["passenger_vehicle",2]],[]]]')) }
-  describe '#has_bike?' do 
+  response = JSON.parse('["40.684626","-73.938369","JEFFERSON AVENUE","MARCUS GARVEY BOULEVARD",[1,1,3,[0,0],[1,0],[1,0],[0,0],[2,0],[],[]],2,[0,1,2,[0,0],[0,0],[0,0],[0,0],[0,0],[["passenger_vehicle",1],["taxi_vehicle",1]],[]],2,[0,1,2,[0,0],[0,0],[0,0],[0,0],[0,0],[["van",1],["passenger_vehicle",1]],[]],[0,1,2,[0,0],[1,0],[1,0],[0,0],[2,0],[["sport_utility_station_wagon",1],["passenger_vehicle",2],["unknown",1]],[]],[1,1,4,[0,0],[0,0],[2,0],[0,0],[2,0],[["van",1],["taxi_vehicle",1]],[]],1,[1,1,2,[1,0],[0,0],[0,0],[0,0],[1,0],[["bicycle",1],["motorcycle",1]],[]],6,[1,1,4,[0,0],[0,0],[2,0],[0,0],[2,0],[["passenger_vehicle",2]],[]]]')
+
+  let(:new_traffic_incident) { TrafficIncident.new(latitude: response[0], longitude: response[1]) }
+  describe '.has_bike?' do 
     it "should return true if a bike is involved" do
-      expect( new_traffic_incident ).to have_bike
+      expect( TrafficIncident.has_bike?(response) ).to eq true
       # expect( new_traffic_incident.hasbike? ).to eq true
     end
-    describe "#latitude" do
+    describe "new traffic incident" do
       it "has latitude coords" do
         expect( new_traffic_incident.latitude ).to eq 40.684626
       end
-    end
-    describe "#longitude" do 
       it "has longitude coords" do 
         expect( new_traffic_incident.longitude ).to eq -73.938369
       end
     end
     describe ".load" do
       it "should load incidents" do
-        incidents = TrafficIncident.load
+        incidents = TrafficIncident.fetch
         expect( incidents ).to be_a(Array)
-        expect( incidents.first ).to be_a(TrafficIncident)
       end
     end
     describe "#bike_accidents" do 
