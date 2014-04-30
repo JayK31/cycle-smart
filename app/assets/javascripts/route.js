919c8659cca12db3964f97d53a15725e39ab0f44
isaacsaqib <isaac.saqib@stonybrook.edu>
2014-04-30 16:34:45 -0400

    adding routes stuff

--- app/assets/javascripts/route.js
+++ app/assets/javascripts/route.js
@@ -1,8 +1,17 @@
 
+
+
 $(document).ready(function(){
 
-  $("#create-route-button").hide();
-  $("#create-route-button").on('click', function(){
-    $("#create-route-button").slideDown();
+  $("#form-route").hide();
+  $("#create-route-button").append($("<button>").attr("id","route-formdrop").text("Create Route"));
+  $("#route-formdrop").on('click', function(){
+    $("#form-route").slideDown();
   });
+
+
+  $("#route-trigger").on('click', function(event){
+    event.preventDefault();
+  });
+});
 
