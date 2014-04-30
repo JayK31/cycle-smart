7fe2ea30702ddeb2539a255b9a24644c942bc466
tliounyc <tom.liou@gmail.com>
2014-04-30 10:45:46 -0400

    added empty functionality to directions div

--- app/assets/javascripts/directions.js
+++ app/assets/javascripts/directions.js
@@ -1,11 +1,7 @@
 function getDirections(){
   return {
-      // Will use commented out origin/destination when form/event
-      //listener is built, hardcoding for now.
       origin: $("#directions-origin").val(),
       destination: $("#directions-destination").val(),
-      // origin: " 61 Wythe Ave, Brooklyn, NY 11249",
-      // destination: "10 E. 21st St., New York, NY",
       provideRouteAlternatives: false,
       travelMode: google.maps.TravelMode.BICYCLING,
       unitSystem: google.maps.UnitSystem.IMPERIAL
@@ -14,10 +10,7 @@ function getDirections(){
 
 function renderDirections(){
 
-    console.log("HELLO FROM INSIDE THE FUNCTION")
-
     direction.route(getDirections(), function(response){
-      console.log(response);
       render = new google.maps.DirectionsRenderer();
       render.setMap(map);
       render.setPanel(document.getElementById("directions"));
@@ -28,6 +21,7 @@ function renderDirections(){
 $(document).ready(function(){
   $("#directions-trigger").on('click', function(event){
     event.preventDefault();
+    $("#directions").empty();
     renderDirections();
   });
 });
