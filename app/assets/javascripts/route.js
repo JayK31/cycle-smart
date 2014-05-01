cbc4743f1864fb2a8e7fe0c917563f844de5e42f
isaacsaqib <isaac.saqib@stonybrook.edu>
2014-05-01 16:00:27 -0400

    css

--- app/assets/javascripts/route.js
+++ app/assets/javascripts/route.js
@@ -2,15 +2,17 @@
 
 $(document).ready(function(){
 
+
+
   $("#form-route").hide();
   $("#create-route-button").append($("<button>").attr("id","route-formdrop").text("Create Route"));
   $("#route-formdrop").on('click', function(){
     $("#form-route").slideDown();
   });
 
+  $("#route-trigger"  ).on('click', function(event){
+    $(preventDefault);
 
-  $("#route-trigger").on('click', function(event){
-    event.preventDefault();
   });
 
   $.ajax({
