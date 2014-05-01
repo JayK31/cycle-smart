b943b4d42b1f07f6170994b86d82e6333a43e2bc
isaacsaqib <isaac.saqib@stonybrook.edu>
2014-05-01 16:59:19 -0400

    css changes

--- app/assets/javascripts/route.js
+++ app/assets/javascripts/route.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     $("#form-route").slideDown();
   });
 
-  $("#route-trigger"  ).on('click', function(event){
+  $("#route-trigger").on('click', function(event){
     $(preventDefault);
 
   });
@@ -24,6 +24,18 @@ $(document).ready(function(){
       $("#route-list").append("<li>" + route.start_point + " to " + route.end_point + "</li>");
     });
   });
+
+
+  // $("#create-route-button").mouseover(function(){
+  //   $("#route-formdrop").css("text-shadow", "0px 1px 1px #4d4d4d");
+  //   $("#route-formdrop").css("color","#222");
+
+  // });
+
+  // $("#create-route-button").mouseleave(function(){
+  //   $("#route-formdrop").css("color", "red");
+  // });
+
 
 });
 
