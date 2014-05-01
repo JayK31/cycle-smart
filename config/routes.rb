b6b2860596be3e4d6bf8c114d76b9e7d47fdfd26
JayK31 <jay.kaye31@gmail.com>
2014-05-01 16:54:08 -0400

    added groupon route to #index

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get "/station" => "station#index"
   get "/shop" => "shop#index"
   get "tips" => "tips#index"
+  get "/groupon" => "groupon#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
